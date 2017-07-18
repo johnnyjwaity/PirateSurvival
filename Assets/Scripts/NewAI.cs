d0d562033b658415cd0cecb16d32601788ae4d37
JohnnyWaity <johnnwaity@gmail.com>
2017-07-18 18:07:14 -0500

    NewAI Second Update

--- Assets/Scripts/NewAI.cs
+++ Assets/Scripts/NewAI.cs
@@ -30,6 +30,7 @@ public class NewAI : MonoBehaviour {
 
     // The path to follow
     public Path path;
+	public bool hasPath;
 
     // Waypoints To Go To
     private int nextPoint;
@@ -39,8 +40,14 @@ public class NewAI : MonoBehaviour {
     public float Head;
     public float Feet;
 
+	// Finds To See If can copy path
+	private NewAI[] enemies;
 
 
+	public ForceMode2D fmode;
+
+	private float LastNodeDistance;
+	private float SecondLastDistance;
 
 
 
@@ -49,11 +56,38 @@ public class NewAI : MonoBehaviour {
         rb = GetComponent<Rigidbody2D>();
         seeker = GetComponent<Seeker>();
         anim = GetComponent<Animator>();
-        newPath();
+
+		enemies = FindObjectsOfType<NewAI> ();
+		var closestDistance = 99999999f;
+		NewAI closestEnemy = null;
+		foreach (NewAI enemy in enemies) {
+			if (enemy.gameObject.name != gameObject.name) {
+				var playerDistanceFromEnemy = Vector3.Distance (enemy.gameObject.transform.position, target.transform.position);
+				var playerDistance = Vector3.Distance (gameObject.transform.position, target.transform.position);
+				var enemyDistance = Vector3.Distance (gameObject.transform.position, enemy.gameObject.transform.position);
+				if (playerDistanceFromEnemy <= playerDistance && enemy.hasPath && enemyDistance < closestDistance) {
+					var hit = Physics2D.Linecast(transform.position, enemy.gameObject.transform.position, ignore);
+					if (!hit) {
+						closestDistance = enemyDistance;
+						closestEnemy = enemy;
+					}
+				}
+			}
+		}
+		if (closestEnemy != null) {
+			path = closestEnemy.path;
+			nextPoint = closestEnemy.nextPoint;
+			hasPath = true;
+			Debug.Log ("Path Came From Existing");
+		} else {
+			newPath ();
+		}
 	}
 	
 	// Update is called once per frame
 	void Update () {
+
+
         if (simpleMovement)
         {
             Vector3 fromPosition = transform.position;
@@ -70,18 +104,33 @@ public class NewAI : MonoBehaviour {
         if(nextPoint >= path.vectorPath.Count)
         {
             rb.velocity = Vector3.zero;
+			hasPath = false;
+			Debug.Log ("Hit Return");
             return;
         }
+
+
+		var playerDistanceToLastNode = Vector3.Distance (target.transform.position, path.vectorPath [path.vectorPath.Count-1]);
+		var playerDistanceSecondToLastNode = Vector3.Distance (target.transform.position, path.vectorPath [path.vectorPath.Count-2]);
+		if (playerDistanceToLastNode - LastNodeDistance >= 1) {
+			var wayCast = Physics2D.Linecast (path.vectorPath [path.vectorPath.Count - 1], target.transform.position, ignore);
+			if (!wayCast) {
+				path.vectorPath.Add (target.transform.position);
+			}
+		}
+		LastNodeDistance = playerDistanceToLastNode;
+		SecondLastDistance = playerDistanceSecondToLastNode;
 
         Vector3 footPosition = transform.position;
         footPosition.y -= Feet;
         Vector3 dir = (path.vectorPath[nextPoint] - footPosition).normalized;
         rb.velocity = dir * speed;
 
-        if(Vector3.Distance(footPosition, path.vectorPath[nextPoint]) < 0.02f)
+        if(Vector3.Distance(footPosition, path.vectorPath[nextPoint]) < 0.2f)
         {
             if(nextPoint+1 <= path.vectorPath.Count)
             {
+				
                 nextPoint++;
             }
         }
@@ -95,5 +144,7 @@ public class NewAI : MonoBehaviour {
     {
         path = p;
         nextPoint = 0;
+		hasPath = true;
+		Debug.Log ("Path Came From New");
     }
 }
