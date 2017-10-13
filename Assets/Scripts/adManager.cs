6d733fd2557ecdd91b11e2d0882a85c1319eb7e4
JohnnyWaity <johnnwaity@gmail.com>
2017-10-12 23:17:49 -0500

    Updated To 2017.2

--- Assets/Scripts/adManager.cs
+++ Assets/Scripts/adManager.cs
@@ -7,44 +7,44 @@ using UnityEngine.Advertisements;
 
 public class adManager : MonoBehaviour {
 
-	private dabloonManager dm;
+	//private dabloonManager dm;
 
-	void Start(){
-		Advertisement.Initialize ("1510127", false);
-		Debug.Log ("Unity Ads initialized: " + Advertisement.isInitialized);
-		Debug.Log ("Unity Ads is supported: " + Advertisement.isSupported);
+	//void Start(){
+	//	Advertisement.Initialize ("1510127", false);
+	//	Debug.Log ("Unity Ads initialized: " + Advertisement.isInitialized);
+	//	Debug.Log ("Unity Ads is supported: " + Advertisement.isSupported);
 
-		dm = FindObjectOfType<dabloonManager> ();
+	//	dm = FindObjectOfType<dabloonManager> ();
 
-	}
+	//}
 
-	public void ShowRewardedAd()
-	{
-		if (Advertisement.IsReady("rewardedVideo"))
-		{
-			var options = new ShowOptions { resultCallback = HandleShowResult };
-			Advertisement.Show("rewardedVideo", options);
-		}
-	}
+	//public void ShowRewardedAd()
+	//{
+	//	if (Advertisement.IsReady("rewardedVideo"))
+	//	{
+	//		var options = new ShowOptions { resultCallback = HandleShowResult };
+	//		Advertisement.Show("rewardedVideo", options);
+	//	}
+	//}
 
-	private void HandleShowResult(ShowResult result)
-	{
-		switch (result)
-		{
-		case ShowResult.Finished:
-			Debug.Log ("The ad was successfully shown.");
-			dm.AddMoney (20);
-			//
-			// YOUR CODE TO REWARD THE GAMER
-			// Give coins etc.
-			break;
-		case ShowResult.Skipped:
-			Debug.Log("The ad was skipped before reaching the end.");
-			break;
-		case ShowResult.Failed:
-			Debug.LogError("The ad failed to be shown.");
-			break;
-		}
-	}
+	//private void HandleShowResult(ShowResult result)
+	//{
+	//	switch (result)
+	//	{
+	//	case ShowResult.Finished:
+	//		Debug.Log ("The ad was successfully shown.");
+	//		dm.AddMoney (20);
+	//		//
+	//		// YOUR CODE TO REWARD THE GAMER
+	//		// Give coins etc.
+	//		break;
+	//	case ShowResult.Skipped:
+	//		Debug.Log("The ad was skipped before reaching the end.");
+	//		break;
+	//	case ShowResult.Failed:
+	//		Debug.LogError("The ad failed to be shown.");
+	//		break;
+	//	}
+	//}
 
 }
